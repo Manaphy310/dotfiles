0b6f697d82765cd9363b4f69c1728b91ac9676bd
SusumuManabe <59593523+SusumuManabe@users.noreply.github.com>
2025-11-09 15:26:34 +0900

    fix: add lazygit settings :bug:

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -4,7 +4,14 @@ return {
     terminal = {
       win = {
         position = "bottom",
-        height = 0.15, -- 15%の高さ
+        height = 0.15, -- 通常のターミナルは15%の高さ
+      },
+    },
+    lazygit = {
+      win = {
+        position = "float",
+        width = 0.9,
+        height = 0.9,
       },
     },
   },
